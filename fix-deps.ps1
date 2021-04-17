3bef3805523944ab32972bd249b27084225dd10c
河蟹 <16880388+JYYYYYLim@users.noreply.github.com>
2021-04-17 22:37:42 +0800

    Update fix-deps.ps1

--- fix-deps.ps1
+++ fix-deps.ps1
@@ -1,5 +1,3 @@
 invoke-webrequest -uri https://files.pythonhosted.org/packages/6b/47/c14abc08432ab22dc18b9892252efaf005ab44066de871e72a38d6af464b/requests-2.25.1.tar.gz -outfile requests-2.25.1.tar.gz
 new-item -path '.\requestssrc' -itemtype directory
 tar -xzf requests-2.25.1.tar.gz -C requestssrc
-set-location -path .\requestssrc\requests-2.25.1
-py setup.py install
