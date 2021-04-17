9f181c12f3eaac54dc8c536959f75d98bc39fb3f
河蟹 <16880388+JYYYYYLim@users.noreply.github.com>
2021-04-17 22:22:44 +0800

    Delete fix-deps.ps1

--- fix-deps.ps1
+++
@@ -1,4 +0,0 @@
-invoke-webrequest -uri https://bootstrap.pypa.io/get-pip.py -outfile get-pip.py
-$env:PYTHONWARNINGS='ignore::UserWarning:setuptools.distutils_patch:26'
-py .\get-pip.py
-pip install requests
