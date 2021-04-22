35faa183c53a56c49bc0f8724e2f9447da36ec3f
河蟹 <16880388+JYYYYYLim@users.noreply.github.com>
2021-04-23 01:08:53 +0800

    Update attendance.py

--- attendance.py
+++ attendance.py
@@ -1,20 +1,19 @@
 import requests
 import json
 
+#>> INITIALIZING SESSION
 s=requests.Session()
-spoofAgent='Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Mobile Safari/537.36'
 ContentType='application/x-www-form-urlencoded'
 
-## USER/PWD WHILE LOOP. ##
+#>> initializing loop flags
+authStatus,resubmitLoop,code,validFlag='nul',1,'',0
 
-#>> AUTH HANDSHAKE PARTITION 
 #>> CREDENTIALS POST SENT. TGT CREATED ON N201. CRED ENCODING DISREGARDED
 try:
     credCheck=open('./settings.json','r')
     pass
     credCheck.close()
 except IOError: 
-    authStatus='nul'
     if authStatus=='nul': print('settings.json not found. Defaulting to manual entry\n')
     while authStatus!=int(201):
         usr=str(input('Enter your TP. no...   '))
@@ -32,51 +31,47 @@ else:
         usr=json.load(optFile)['secrets']['usr']
     with open('settings.json','r') as optFile:
         pwd=json.load(optFile)['secrets']['pwd']
-    #apparently the fileObject closes itself after every read lmfao
+    # apparently the fileObject closes itself after every read lmfao
+    
     HTTPPost=s.post('https://cas.apiit.edu.my/cas/v1/tickets',data={'username': usr, 'password': pwd})
     print('Previous session detected, reusing saved credentials.')
 
-
-############# CREDENTIALS APPROVED, TGT LINK CREATED ##########################
-logonTicket=json.loads(str(HTTPPost.headers).replace("'", '"'))['Location']
-#print('logonTicket:'+ logonTicket)
-###############################################################################
-
+#>> CREDENTIALS APPROVED, TGT CREATED 
+#>> ADVANCING THROUGH DOUBLE AUTH HANDSHAKE
+logonTicket=json.loads(str(HTTPPost.headers).replace("'", '"'))["Location"]
 initHandshake=s.post(logonTicket,headers= {"content-type": ContentType},params= {'service': 'https://cas.apiit.edu.my'})
-#print('initHandshake:'+ initHandshake)
 authTicket=s.get('https://cas.apiit.edu.my/cas/p3/serviceValidate',params={'format': 'json', 'service': 'https://cas.apiit.edu.my', 'ticket': initHandshake.text})
 print('Logged in as: '+str(json.loads(authTicket.text)['serviceResponse']['authenticationSuccess']['attributes']['givenName']).replace("'",'')+'\n\n')
 
-##logon successful, build HTTP request with graphQL P/L. ##
+#>> logon successful, build HTTP request with graphQL P/L.
 endpoint='https://attendix.apu.edu.my/graphql'
 graphqlContent='application/json'
 
 accessCtlReqHeaders='content-type,ticket,x-amz-user-agent,x-api-key'
 accessCtlReqMethod='POST'
 CtlReq=s.options(endpoint,headers={"host": 'attendix.apu.edu.my',"path": '/graphql' , "sec-fetch-dest": 'empty', "sec-fetch-mode": 'cors', "sec-fetch-site": 'same-site', "access-control-request-headers": accessCtlReqHeaders, "access-control-request-method": accessCtlReqMethod})
-#print(finCtl.headers)
-
-# WIREFRAME CHECK
-code='nul'
-while code=='nul':
-    try:
-        code=int(input('Enter the 3-digit passcode. '))
-    except: 
-        print('Invalid response.')
-
-############################### IN CASE OF E401, RECHECK. ####################################
 agent='aws-amplify/1.0.1'
 key='da2-dv5bqitepbd2pmbmwt7keykfg4'
-payload={"operationName":"updateAttendance","variables":{"otp":code},"query": "mutation updateAttendance($otp: String!) {\n  updateAttendance(otp: $otp) {\n    id\n    attendance\n    classcode\n    date\n    startTime\n    endTime\n    classType\n    __typename\n  }\n}\n"}
-##############################################################################################
 
-#>> SERVICE QUERY
-attendix=s.post(logonTicket,headers={"content-type": ContentType},params={'service': 'https://api.apiit.edu.my/attendix'}).text
-#print('attendix:'+ attendix)
+## catch invalid attendance code, saving invalid http calls. breaks on valid hit
+while resubmitLoop==1:
+    while validFlag!=1:
+        try:
+            code=int(input('Enter the 3-digit passcode. '))
+            if len(str(code))!=3:
+                raise lenError("Input chars >3")
+            break
+        except:
+            print('Invalid response!\n')
 
-attendUpdate=s.post(endpoint,headers={"host": 'attendix.apu.edu.my', "path": '/graphql', "content-type": graphqlContent, "sec-fetch-dest": 'empty', "sec-fetch-mode": 'cors', "sec-fetch-site": 'same-site', "ticket": attendix, "x-amz-user-agent": agent, "x-api-key": key},json=payload)
-feedbackMessage=((json.loads(str(attendUpdate.text))['errors'])[0])
-#print(json.loads(str(attendUpdate.text)))
-print(feedbackMessage['message'])
-
-###!!! ON FAIL, RESUBMIT SERVICE QUERY (ATTENDIX). ANALYZE BY MONDAY
+    # the fuck is this? at least it works only after this POST
+    attendix=s.post(logonTicket,headers={"content-type": ContentType},params={'service': 'https://api.apiit.edu.my/attendix'}).text
+    payload={"operationName":"updateAttendance","variables":{"otp":code},"query": "mutation updateAttendance($otp: String!) {\n  updateAttendance(otp: $otp) {\n    id\n    attendance\n    classcode\n    date\n    startTime\n    endTime\n    classType\n    __typename\n  }\n}\n"}
+    attendUpdate=s.post(endpoint,headers={"host": 'attendix.apu.edu.my', "path": '/graphql', "content-type": graphqlContent, "sec-fetch-dest": 'empty', "sec-fetch-mode": 'cors', "sec-fetch-site": 'same-site', "ticket": attendix, "x-amz-user-agent": agent, "x-api-key": key},json=payload)
+    try:
+        classtyp=str(json.loads(attendUpdate.text)['data']['updateAttendance']['classType'])
+        feedbackMessage="Success! Logged attendance for"+classtyp+': '+json.loads(str(attendUpdate.text))['data']['updateAttendance']['classcode']
+        exit()
+    except:
+        feedbackMessage=((json.loads(str(attendUpdate.text))['errors'])[0])
+        print(feedbackMessage['message']+'\n')
